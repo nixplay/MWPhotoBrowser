3d17db7369f81e293662c911669e0c5c2454e2c2
James Kong <james.kong@nixplay.com>
2017-06-23 17:50:49 +0800

    Added new. Classed

---
+++ Pod/Classes/PortraitNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  PortraitNavigationController.h
+//  Nixplay
+//
+//  Created by James Kong on 23/6/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PortraitNavigationController : UINavigationController
+
+@end
---
+++ Pod/Classes/PortraitNavigationController.m
@@ -0,0 +1,51 @@
+//
+//  PortraitNavigationController.m
+//  Nixplay
+//
+//  Created by James Kong on 23/6/2017.
+//
+//
+
+#import "PortraitNavigationController.h"
+
+@interface PortraitNavigationController ()
+
+@end
+
+@implementation PortraitNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (BOOL)shouldAutorotate
+{
+    return NO;
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
+{
+    return 1 << UIInterfaceOrientationPortrait;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+@end
