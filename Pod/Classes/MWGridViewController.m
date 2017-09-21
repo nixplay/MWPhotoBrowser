78f3461a4e8f8c6bf77f3900fc7a965b8294fb16
James Kong <james.kong@nixplay.com>
2017-09-21 16:13:33 +0800

    Set safe area inset to grid view

--- Pod/Classes/MWGridViewController.m
+++ Pod/Classes/MWGridViewController.m
@@ -109,11 +109,21 @@
     
 }
 
+- (void)viewSafeAreaInsetsDidChange {
+    [super viewSafeAreaInsetsDidChange];
+    UIEdgeInsets contentInset = self.collectionView.contentInset;
+    contentInset.left = self.view.safeAreaInsets.left;
+    contentInset.right = self.view.safeAreaInsets.right;
+    self.collectionView.contentInset = contentInset;
+}
+
 - (void)performLayout {
     UINavigationBar *navBar = self.navigationController.navigationBar;
     CGSize statubarSize = [[UIApplication sharedApplication] statusBarFrame].size;
     if(SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"11.0")) {
-        self.collectionView.contentInset = UIEdgeInsetsMake(navBar.frame.origin.y - statubarSize.height, 0, [self getBottom], 0);
+        if(@available(iOS 11, *)){
+            self.collectionView.contentInset = UIEdgeInsetsMake(navBar.frame.origin.y - statubarSize.height, self.view.safeAreaInsets.left, [self getBottom], self.view.safeAreaInsets.right);
+        }
     }else{
         self.collectionView.contentInset = UIEdgeInsetsMake(navBar.frame.origin.y + navBar.frame.size.height + [self getGutter], 0, [self getBottom], 0);
     }
@@ -127,7 +137,7 @@
 #pragma mark - Layout
 
 - (CGFloat)getColumns {
-    if ((UIInterfaceOrientationIsPortrait(self.interfaceOrientation))) {
+    if ((UIInterfaceOrientationIsPortrait([[UIApplication sharedApplication] statusBarOrientation]))) {
         return _columns;
     } else {
         return _columnsL;
@@ -135,7 +145,7 @@
 }
 
 - (CGFloat)getMargin {
-    if ((UIInterfaceOrientationIsPortrait(self.interfaceOrientation))) {
+    if ((UIInterfaceOrientationIsPortrait([[UIApplication sharedApplication] statusBarOrientation]))) {
         return _margin;
     } else {
         return _marginL;
@@ -143,7 +153,7 @@
 }
 
 - (CGFloat)getGutter {
-    if ((UIInterfaceOrientationIsPortrait(self.interfaceOrientation))) {
+    if ((UIInterfaceOrientationIsPortrait([[UIApplication sharedApplication] statusBarOrientation]))) {
         return _gutter;
     } else {
         return _gutterL;
@@ -199,8 +209,14 @@
     CGFloat margin = [self getMargin];
     CGFloat gutter = [self getGutter];
     CGFloat columns = [self getColumns];
-    CGFloat value = floorf(((self.view.bounds.size.width - (columns - 1) * gutter - 2 * margin) / columns));
-    return CGSizeMake(value, value);
+    
+    if(@available(iOS 11, *)){
+        CGFloat value = floorf((((self.view.bounds.size.width-self.view.safeAreaInsets.left-self.view.safeAreaInsets.right) - (columns - 1) * gutter - 2 * margin) / columns));
+        return CGSizeMake(value, value);
+    }else{
+        CGFloat value = floorf(((self.view.bounds.size.width - (columns - 1) * gutter - 2 * margin) / columns));
+        return CGSizeMake(value, value);
+    }
 }
 
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
