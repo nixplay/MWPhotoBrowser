47b35d3260ede57c7b68f829624c72826a29e517
James Kong <james.kong@nixplay.com>
2018-02-09 12:00:30 +0800

    Created protocol

--- Pod/Classes/MWZoomingScrollView.h
+++ Pod/Classes/MWZoomingScrollView.h
@@ -12,11 +12,29 @@
 #import "MWTapDetectingView.h"
 @import AVKit;
 @import AVFoundation;
-
 @class MWPhotoBrowser, MWPhoto, MWCaptionView;
 
-@interface MWZoomingScrollView : UIScrollView <UIScrollViewDelegate, MWTapDetectingImageViewDelegate, MWTapDetectingViewDelegate> {
-    @protected
+@protocol MWZoomingScrollView <NSObject>
+@required
+- (void)onPlaybackTimeCheckerTimer;
+- (id)initWithPhotoBrowser:(MWPhotoBrowser *)browser;
+- (void)setupVideoPreviewUrl:(NSURL*)url avurlAsset:(AVURLAsset*)avurlAsset photoImageViewFrame:(CGRect)photoImageViewFrame;
+- (void)displayImage;
+- (void)displayImageFailure;
+- (void)setMaxMinZoomScalesForCurrentBounds;
+- (void)prepareForReuse;
+- (BOOL)displayingVideo;
+- (void)setImageHidden:(BOOL)hidden;
+- (void)displaySubView:(CGRect)photoImageViewFrame;
+- (void)onVideoTapped;
+- (void)startPlaybackTimeChecker;
+- (void)stopPlaybackTimeChecker;
+- (void)seekVideoToPos:(CGFloat)pos;
+- (void)resetPlayer;
+@end
+
+@interface MWZoomingScrollView : UIScrollView <MWZoomingScrollView, UIScrollViewDelegate, MWTapDetectingImageViewDelegate, MWTapDetectingViewDelegate> {
+@protected
     AVPlayer *player;
     AVPlayerItem *playerItem;
     AVPlayerLayer *playerLayer;
@@ -46,19 +64,8 @@
 @property (nonatomic , strong) NSString *tempVideoPath;
 @property (nonatomic , strong) AVAsset *asset;
 @property (assign, nonatomic) BOOL isPlaying;
+@property (assign, nonatomic) BOOL isReadyToPlay;
 
 
-- (id)initWithPhotoBrowser:(MWPhotoBrowser *)browser;
-- (void)displayImage;
-- (void)displayImageFailure;
-- (void)setMaxMinZoomScalesForCurrentBounds;
-- (void)prepareForReuse;
-- (BOOL)displayingVideo;
-- (void)setImageHidden:(BOOL)hidden;
-- (void)displaySubView:(CGRect)photoImageViewFrame;
-- (void) onVideoTapped;
-- (void)startPlaybackTimeChecker;
-- (void)stopPlaybackTimeChecker;
-- (void)seekVideoToPos:(CGFloat)pos;
-- (void)resetPlayer;
+
 @end
